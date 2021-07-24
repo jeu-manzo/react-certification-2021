1a23de99baec63cd5789465c8b6aa3113cd682dc
Jeu Manzo <jeunettemanzo@gmail.com>
2021-07-24 17:10:50 -0500

    install styled-components and create global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import App from './components/App';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #F9F3FD;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 );
