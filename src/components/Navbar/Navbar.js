f249066360b991a49704f73bc3b9485dbf1e2fc1
Jeu Manzo <jeunettemanzo@gmail.com>
2021-07-26 13:22:53 -0500

    separate navbar styled

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { FaUserCircle } from 'react-icons/fa';
 
 import Logo from '../Logo';
-
-const Nav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 10px;
-`;
-
-const Column = styled.div`
-  display: flex;
-  align-items: center;
-  svg {
-    font-size: 32px;
-    margin-left: 12px;
-    color: #1b1525;
-  }
-`;
+import { Nav, Column } from './Navbar.styled';
 
 function Navbar() {
   return (
---
+++ src/components/Navbar/Navbar.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 10px;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    font-size: 32px;
+    margin-left: 12px;
+    color: #1b1525;
+  }
+`;
