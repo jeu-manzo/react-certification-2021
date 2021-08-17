d156a9be754855a09ad521934ff0289d47545374
Jeu Manzo <jeunettemanzo@gmail.com>
2021-08-16 20:31:02 -0500

    fix app test warnings

--- src/components/App/app.test.js
+++ src/components/App/app.test.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { delay, makeFetchResponse } from '../../utils/helpers';
 import ThemeControlProvider from '../../providers/Theme';
 import App from './index';
 import mockVideos from '../../utils/youtube-videos-mock';
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve(mockVideos),
-  })
-);
-
-beforeEach(() => {
-  fetch.mockClear();
-  render(
-    <ThemeControlProvider>
-      <App />
-    </ThemeControlProvider>
+const waitFinisUpdating = async () => {
+  const firstVideoImage = await screen.findByAltText(
+    mockVideos.items[0].snippet.title,
+    {},
+    { timeout: 8000 }
   );
-});
+  expect(firstVideoImage).toBeInTheDocument();
+};
 
 describe('App', () => {
-  test('should have a navbar', () => {
-    const navbar = screen.getByTestId(/navbar/i);
-    expect(navbar).toBeInTheDocument();
+  beforeEach(() => {
+    global.fetch = jest.fn().mockImplementation(async () => {
+      await delay(2);
+      return makeFetchResponse(mockVideos);
+    });
+
+    render(
+      <ThemeControlProvider>
+        <App />
+      </ThemeControlProvider>
+    );
   });
 
-  test('should have a home view', () => {
+  test('should have a navbar', async () => {
+    const navbar = screen.getByTestId(/navbar/i);
+    expect(navbar).toBeInTheDocument();
+    await waitFinisUpdating();
+  });
+
+  test('should have a home view', async () => {
     const homepage = screen.getByTestId(/homepage/i);
     expect(homepage).toBeInTheDocument();
+
+    await waitFinisUpdating();
   });
 
   test('should have search results', async () => {
@@ -35,10 +46,10 @@ describe('App', () => {
     fireEvent.change(input, { target: { value: 'vaporwave' } });
 
     const button = screen.getByTestId('searchButton');
+
     fireEvent.click(button);
 
-    const firstVideoImage = await screen.findByAltText(mockVideos.items[0].snippet.title);
-    expect(firstVideoImage).toBeInTheDocument();
+    await waitFinisUpdating();
 
     const firstVideoTitle = await screen.findByText(mockVideos.items[0].snippet.title);
     expect(firstVideoTitle).toBeInTheDocument();
